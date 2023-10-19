2d73255ea7fd0372f3e70d0780f3c17d1ee2706b
FOXBOOK\menjm <menjmenj@gmail.com>
2023-10-20 01:47:49 +0300

    Fix SectionSpawner

--- Assets/Scripts/SectionSpawner.cs
+++ Assets/Scripts/SectionSpawner.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Microsoft.MixedReality.Toolkit.UI;
 using Microsoft.MixedReality.Toolkit.Utilities;
 using UnityEngine;
 
@@ -9,7 +8,7 @@ public class SectionSpawner : MonoBehaviour
     [SerializeField] private GridObjectCollection _bottomGrid;
     [SerializeField] private BombConfig _bombConfig;
 
-    private readonly List<BaseSection> _sectionList = new();
+    private readonly List<GameObject> _sectionList = new();
 
     public void Start()
     {
@@ -19,16 +18,15 @@ public class SectionSpawner : MonoBehaviour
             var sectionPrefab = PopRandomBaseSection(_sectionList);
             var section = Instantiate(sectionPrefab).transform;
 
-            section.rotation = Quaternion.Euler(-90, 0, i < 6 ? 0 : 180);
-            section.parent = i < 6 ? _topGrid.transform.GetChild(i) : _bottomGrid.transform.GetChild(i - 6);
-            section.localPosition = new Vector3(0, 0, 0.0115f);
-            
-            section.parent.gameObject.GetComponent<Interactable>().OnClick
-                .AddListener(section.GetComponent<BaseSection>().Interact);
+            section.parent = (i < 6 ? _topGrid : _bottomGrid).transform;
+            section.localRotation = section.rotation;
+            section.localPosition = Vector3.zero;
         }
+        _topGrid.UpdateCollection();
+        _bottomGrid.UpdateCollection();
     }
-
-    private BaseSection PopRandomBaseSection(IList<BaseSection> list)
+    
+    private GameObject PopRandomBaseSection(IList<GameObject> list)
     {
         var randomIndex = Random.Range(0, list.Count);
         var section = list[randomIndex];
@@ -36,10 +34,10 @@ public class SectionSpawner : MonoBehaviour
         return section;
     }
 
-    private void FillSectionList(ICollection<BaseSection> list, int listLength = 12)
+    private void FillSectionList(ICollection<GameObject> list, int listLength = 12)
     {
-        list.Add(_bombConfig.TimerSection);
+        list.Add(_bombConfig.TimerSection.gameObject);
         foreach (var section in _bombConfig.Sections) list.Add(section);
-        for (var i = list.Count; i < listLength; i++) list.Add(_bombConfig.EmptySection);
+        for (var i = list.Count; i < listLength; i++) list.Add(_bombConfig.EmptySection.gameObject);
     }
 }

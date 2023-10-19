a0adf6767318963d48b58e3e967a988f5d49ed16
FOXBOOK\menjm <menjmenj@gmail.com>
2023-10-19 23:45:52 +0300

    Update Section spawner

--- Assets/Scripts/SectionSpawner.cs
+++ Assets/Scripts/SectionSpawner.cs
@@ -1,8 +1,6 @@
-using Microsoft.MixedReality.Toolkit.Utilities;
-using Microsoft.MixedReality.Toolkit.UI;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities;
 using UnityEngine;
 
 public class SectionSpawner : MonoBehaviour
@@ -11,40 +9,37 @@ public class SectionSpawner : MonoBehaviour
     [SerializeField] private GridObjectCollection _bottomGrid;
     [SerializeField] private BombConfig _bombConfig;
 
-    private BaseSection[] _sections = new BaseSection[12];
+    private readonly List<BaseSection> _sectionList = new();
 
     public void Start()
     {
-        int sectionIndex = Random.Range(0, 12);
-        _sections[sectionIndex] = Instantiate(_bombConfig.TimerSection);
-        _sections[sectionIndex].transform.localRotation = Quaternion.Euler(sectionIndex > 5 ? 90 : -90, 0, 0);
-        if (sectionIndex <= 5) _sections[sectionIndex].transform.parent = _topGrid.transform.GetChild(sectionIndex).transform;
-        else _sections[sectionIndex].transform.parent = _bottomGrid.transform.GetChild(sectionIndex - 6).transform;
-        _sections[sectionIndex].transform.localPosition = new Vector3(0, 0, 0.0115f);
-
-        int counter = _bombConfig.Sections.Length;
-        while (counter > 0)
+        FillSectionList(_sectionList);
+        for (var i = 0; i < 12; i++)
         {
-            sectionIndex = Random.Range(0, 12);
-            if (_sections[sectionIndex] != null) continue;
-            _sections[sectionIndex] = Instantiate(_bombConfig.Sections[--counter]);
-            _sections[sectionIndex].transform.localRotation = Quaternion.Euler(sectionIndex > 5 ? 90 : -90, 0, 0);
-            if (sectionIndex <= 5) _sections[sectionIndex].transform.parent = _topGrid.transform.GetChild(sectionIndex).transform;
-            else _sections[sectionIndex].transform.parent = _bottomGrid.transform.GetChild(sectionIndex - 6).transform;
-            _sections[sectionIndex].transform.localPosition = new Vector3(0, 0, 0.0115f);
+            var sectionPrefab = PopRandomBaseSection(_sectionList);
+            var section = Instantiate(sectionPrefab).transform;
 
-            _sections[sectionIndex].transform.parent.gameObject.GetComponent<Interactable>().OnClick.AddListener(_sections[sectionIndex].GetComponent<BaseSection>().Interact);
-        }
-
-        for (int i = 0; i < _topGrid.transform.childCount + _bottomGrid.transform.childCount; i++)
-        {
-            if (_sections[i] != null) continue;
+            section.rotation = Quaternion.Euler(-90, 0, i < 6 ? 0 : 180);
+            section.parent = i < 6 ? _topGrid.transform.GetChild(i) : _bottomGrid.transform.GetChild(i - 6);
+            section.localPosition = new Vector3(0, 0, 0.0115f);
             
-            _sections[i] = Instantiate(_bombConfig.EmptySection);
-            _sections[i].transform.localRotation = Quaternion.Euler(i > 5 ? 90 : -90, 0, 0);
-            if (i <= 5) _sections[i].transform.parent = _topGrid.transform.GetChild(i).transform;
-            else _sections[i].transform.parent = _bottomGrid.transform.GetChild(i - 6).transform;
-            _sections[i].transform.localPosition = new Vector3(0, 0, 0.0115f);
+            section.parent.gameObject.GetComponent<Interactable>().OnClick
+                .AddListener(section.GetComponent<BaseSection>().Interact);
         }
+    }
+
+    private BaseSection PopRandomBaseSection(IList<BaseSection> list)
+    {
+        var randomIndex = Random.Range(0, list.Count);
+        var section = list[randomIndex];
+        list.RemoveAt(randomIndex);
+        return section;
+    }
+
+    private void FillSectionList(ICollection<BaseSection> list, int listLength = 12)
+    {
+        list.Add(_bombConfig.TimerSection);
+        foreach (var section in _bombConfig.Sections) list.Add(section);
+        for (var i = list.Count; i < listLength; i++) list.Add(_bombConfig.EmptySection);
     }
 }

40cddae00e400bb3b1faf511fe3258c12c52c4d2
FOXBOOK\menjm <menjmenj@gmail.com>
2023-10-23 17:47:26 +0300

    Moved the countdown to the explosion from the timer section to the bomb script

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    [Range(60, 0)]
-    [SerializeField] private int _waitTime = 5;
-
     public static Bomb Instance;
-    public SectionController SectionController;
+
+    [Range(60, 0)] [SerializeField] private int _waitTime = 5;
+    [Range(0, 60)] [SerializeField] private int _startTimerMinutes = 5;
+    [Range(0, 59)] [SerializeField] private int _startTimerSeconds;
+
     public Phase Phase = Phase.Wait;
 
     private float _timer;
+    public SectionController SectionController;
+    public float BombTimer { get; private set; }
 
     private void Start()
     {
         Instance = this;
         SectionController = new SectionController();
+        BombTimer = _startTimerMinutes * 60 + _startTimerSeconds;
         _timer = Time.time;
     }
 
@@ -24,18 +28,21 @@ public class Bomb : MonoBehaviour
         {
             case Phase.Wait:
                 if (Time.time - _timer < _waitTime)
-                {
                     Debug.Log(Time.time);
-                }
                 else
-                {
                     Phase = Phase.Defuse;
-                }
                 break;
+
+            case Phase.Defuse:
+                if (BombTimer <= 0) Phase = Phase.Explode;
+                BombTimer -= Time.deltaTime;
+                break;
+
             case Phase.Explode:
                 Debug.Log("Dead");
                 Destroy(gameObject);
                 break;
+
             case Phase.Stop:
                 Debug.Log("You win!!!");
                 break;
--- Assets/Scripts/Sections/TimerSection.cs
+++ Assets/Scripts/Sections/TimerSection.cs
@@ -3,33 +3,18 @@ using UnityEngine;
 
 public class TimerSection : BaseSection
 {
-    [Range(0, 60)] [SerializeField] private int _startTimerMinutes = 5;
-    [Range(0, 59)] [SerializeField] private int _startTimerSeconds;
     [SerializeField] private TextMeshProUGUI _text;
-
-    private float _bombTimer;
-
-    private void Start()
-    {
-        _bombTimer = _startTimerMinutes * 60 + _startTimerSeconds;
-        UpdateText();
-    }
 
     private void Update()
     {
-        if (Bomb.Instance.Phase != Phase.Defuse) return;
-        if (_bombTimer <= 0) Bomb.Instance.Phase = Phase.Explode;
-
-        _bombTimer -= Time.deltaTime;
-        UpdateText();
+        UpdateText(Bomb.Instance.BombTimer);
     }
 
-    private void UpdateText()
+    private void UpdateText(float timer)
     {
-        var m = Mathf.FloorToInt(_bombTimer) / 60;
-        var s = Mathf.FloorToInt(_bombTimer) % 60;
-        var ms = Mathf.CeilToInt(_bombTimer % 1 * 100);
-        Debug.Log(_bombTimer);
+        var m = Mathf.FloorToInt(timer) / 60;
+        var s = Mathf.FloorToInt(timer) % 60;
+        var ms = Mathf.CeilToInt(timer % 1 * 100);
         _text.text = $"{m:D2}:{s:D2}:{ms:D2}";
     }
 }
